a45327cf075e27923c5496ca2e995465c320324a
vaibhavpawarsdet <vaibhavpawarsdet@gmail.com>
2023-05-15 00:05:57 +0530

    update all

--- app.js
+++ app.js
@@ -2,19 +2,49 @@ require('dotenv').config();
 
 const express = require('express');
 const expressLayout = require('express-ejs-layouts');
+const methodOverride = require('method-override');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+
+const connectDB = require('./config/db');
+const { isActiveRoute } = require('./helpers/routeHelpers');
 
 const app = express();
 const PORT = 5000 || process.env.PORT;
+  
+// Connect to DB
+connectDB();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(methodOverride('_method'));
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGODB_URI
+  }),
+  //cookie: { maxAge: new Date ( Date.now() + (3600000) ) } 
+}));
 
 app.use(express.static('public'));
 
-//templating engine
+// Templating Engine
 app.use(expressLayout);
 app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
 
-app.use('', require('./routes/index'));
+
+app.locals.isActiveRoute = isActiveRoute; 
+
+
+app.use('/', require('./routes/index'));
+app.use('/', require('./routes/admin'));
 
 app.listen(PORT, ()=> {
-  console.log(`app listening on port ${PORT}`);
-})
+  console.log(`App listening on port ${PORT}`);
+});
