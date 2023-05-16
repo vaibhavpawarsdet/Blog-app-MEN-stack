f75cb56a3be56890f5e4c32a9b0711701616145d
vaibhavpawarsdet <vaibhavpawarsdet@gmail.com>
2023-05-16 23:19:53 +0530

    js updated

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', function(){
-
     const allButtons = document.querySelectorAll('.searchBtn');
     const searchBar = document.querySelector('.searchBar');
     const searchInput = document.getElementById('searchInput');
