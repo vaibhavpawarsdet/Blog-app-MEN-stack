5f5aa64cf1b4ec2b3a88f35933977e396c9fb98c
vaibhavpawarsdet <vaibhavpawarsdet@gmail.com>
2023-05-17 19:55:26 +0530

    js updated

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', function(){
+  
     const allButtons = document.querySelectorAll('.searchBtn');
     const searchBar = document.querySelector('.searchBar');
     const searchInput = document.getElementById('searchInput');
     const searchClose = document.getElementById('searchClose');
-    
+
     for (var i = 0; i < allButtons.length; i++) {
       allButtons[i].addEventListener('click', function() {
         searchBar.style.visibility = 'visible';
